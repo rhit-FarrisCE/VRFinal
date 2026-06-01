b69bf22727a13f91b2c51eb2e55b071ad8fbe0ee
rhit-pereirl <pereirl@rose-hulman.edu>
2026-06-01 22:23:18 +0900

    Update BallExitTrigger.cs

--- Assets/Scripts/BallExitTrigger.cs
+++ Assets/Scripts/BallExitTrigger.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class BallExitTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public float timeBeforeScoring = 3f;
+    private bool hasTriggered = false;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Ball"))
+        {
+            hasTriggered = true;
+            StartCoroutine(ScoreAfterDelay());
+        }
+    }
+
+    private IEnumerator ScoreAfterDelay()
+    {
+        yield return new WaitForSeconds(timeBeforeScoring);
+
+        int pinsDownCount = PinManager.Instance.GetPinsDown();
+        int total = PinManager.Instance.GetTotalPins();
+
+        Debug.Log("Pins knocked down: " + pinsDownCount + " out of " + total + ".");
     }
 
     // Update is called once per frame
