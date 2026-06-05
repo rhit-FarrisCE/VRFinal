b609ceae36ecce6cd6fa34496fed62dc198ea8b1
rhit-pereirl <pereirl@rose-hulman.edu>
2026-06-05 23:05:25 +0900

    Added code for a powerup that makes pins lighter

---
+++ Assets/Scripts/LightPinsPowerup.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal.Internal;
+
+public class LightPinsPowerup : MonoBehaviour
+{
+
+    private List<BowlingPin> pins = new List<BowlingPin>();
+    private static float og_pin_mass = 5f;
+    private bool active = false;
+
+    public void setActive()
+    {
+        foreach (BowlingPin pin in pins)
+        {
+            pin.GetComponent<Rigidbody>().mass = 1;
+        }
+    }
+    public void setInactive()
+    {
+        foreach (BowlingPin pin in pins)
+        {
+            pin.GetComponent<Rigidbody>().mass = og_pin_mass;
+        }
+    }
+
+}
